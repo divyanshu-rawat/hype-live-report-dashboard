5eaadc3598139d65e54533c9b8834b39fd3c832d
Divyanshu <divyanshu.r46956@gmail.com>
2021-06-24 19:11:36 +0200

    Updated directory structure, added util to fetch events, refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React, { useEffect } from 'react';
 
 import styled from '@emotion/styled';
 
+import { fetchData } from './utils/index';
+
 const AppContainer = styled.div``;
 
-export const App: React.FC = () => (
-  <AppContainer>
-    <header className="App-header">
-      <p>Hello </p>
-    </header>
-  </AppContainer>
-);
+export const App: React.FC = () => {
+  useEffect(() => {
+    fetchData().then((events) => {
+      console.log(events);
+    });
+  }, []);
+  return (
+    <AppContainer>
+      <header className="App-header">
+        <p>Hello </p>
+      </header>
+    </AppContainer>
+  );
+};
---
+++ src/utils/index.ts
@@ -0,0 +1,13 @@
+type eventType = {
+  displayName: string;
+  participants: number;
+  revenue: number;
+  sellThrough: number;
+  stock: number;
+};
+
+export const fetchData = async (): Promise<eventType[]> => {
+  const events = await fetch('./data.json');
+  const eventsJson = await events.json();
+  return (eventsJson as { data: eventType[] }).data;
+};
